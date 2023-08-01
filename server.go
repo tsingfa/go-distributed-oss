c484d31ffa70a7512d0fa00ec41b63b3438750f6
tsingfa <tsingfa6@163.com>
2023-08-02 00:20:23 +0800

    fix: adjusts as version is updated

--- server.go
+++ server.go
@@ -1,3 +1,5 @@
+////LISTEN_ADDRESS=:12345 STORAGE_ROOT=~/GolandProjects/go-distributed-oss/tmp go run server.go
+
 package main
 
 import (
@@ -8,6 +10,9 @@ import (
 	"os"
 )
 
+// main 单机版的server端函数
+// 注意：在分布式版本中实现了接口服务与数据服务的解耦，
+// 则二者有各自的main函数，不再使用单机版的main函数。
 func main() {
 	// 定义路由处理函数
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
