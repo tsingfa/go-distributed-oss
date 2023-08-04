f5bf3037833b22f5a9a5c1e50aac4f084b044a66
tsingfa <tsingfa6@163.com>
2023-08-04 08:19:19 +0800

    refactor: optimized the start script and add the function start_rabbitmq_and_es()

--- tools/start_test_env.sh
+++ tools/start_test_env.sh
@@ -7,6 +7,24 @@ export RABBITMQ_SERVER=amqp://test:test@localhost:5672
 export ES_SERVER=localhost:9200
 export GODOSS="/home/tsingfa/GolandProject/go-distributed-oss"
 
+start_rabbitmq_and_es() {
+  # 检查RabbitMQ是否已经启动
+  if ! pgrep -x "rabbitmq-server" > /dev/null; then
+    echo "Starting RabbitMQ..."
+    sudo systemctl start rabbitmq-server
+  else
+    echo "RabbitMQ is already running."
+  fi
+
+  # 检查Elasticsearch是否已经启动
+  if ! pgrep -x "elasticsearch" > /dev/null; then
+    echo "Starting Elasticsearch..."
+    sudo systemctl start elasticsearch
+  else
+    echo "Elasticsearch is already running."
+  fi
+}
+
 # 定义函数 - 启动 dataServer 进程
 start_data_servers() {
     # 监听地址为init_test_env.sh中分配的虚拟地址
@@ -33,6 +51,7 @@ stop_servers() {
 # 判断用户传递的参数并执行相应的操作
 case "$1" in
     "start")
+        start_rabbitmq_and_es
         start_data_servers
         start_api_servers
         ;;
